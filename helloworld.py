025eb9e616f7de609b769481d2d04389acf4444b
Umunatu Sam <samumunatu@gmail.com>
2026-04-07 00:37:56 -0400

    Update helloworld for this

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,18 @@
+<<<<<<< HEAD
 def hello_world(city = "New York City"):
     print(f'Hello World from {city}!')
 
 
 def main():
     hello_world("NYC")
+=======
+def hello_world(state):
+    print(f'Hello World from {state}!')
+
+
+def main():
+    hello_world("New York")
+>>>>>>> 1fd7596 (add state)
 
 
 main()
